fc4ba7b1fadc22ff6852f0fda2a9c90b4f133aec
Rishabh Dewangan <107680241+Rishabh-git10@users.noreply.github.com>
2026-05-10 18:08:26 +0000

    fix(core): enable fault-tolerant execution and incremental result persistence
    Signed-off-by: Rishabh Dewangan <107680241+Rishabh-git10@users.noreply.github.com>

--- core/cmd/obj/benchmarkingjob.py
+++ core/cmd/obj/benchmarkingjob.py
@@ -91,10 +91,15 @@ class BenchmarkingJob:
         self.testcase_controller.build_testcases(test_env=self.test_env,
                                                  test_object=self.test_object)
 
-        succeed_testcases, test_results = self.testcase_controller.run_testcases(self.workspace)
+        def incremental_save(testcase, res, time):
+            self.rank.save([testcase], {testcase.id: (res, time)}, output_dir=self.workspace)
+
+        _, test_results = self.testcase_controller.run_testcases(
+            self.workspace,
+            incremental_save_cb=incremental_save
+        )
 
         if test_results:
-            self.rank.save(succeed_testcases, test_results, output_dir=self.workspace)
             self.rank.plot()
 
     def _parse_config(self, config: dict):
